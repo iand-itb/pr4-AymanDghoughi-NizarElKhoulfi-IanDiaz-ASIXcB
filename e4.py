2fca35d531ca76dafc27bf25ac1f3dc75a6a4f1e
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-11-30 09:11:38 +0100

    ye

---
+++ e4.py
@@ -0,0 +1,11 @@
+BLANC = "██"
+NEGRE = "  "
+
+for i in range(1, 9):
+   for j in range(1, 9):
+       if (i + j) % 2 == 0:
+           print(NEGRE, end="")
+       else:
+           print(BLANC, end="")
+       #print(" ", end="")
+   print()
