2b7e8cfec121d26106600d0ed465429747d4f2e1
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-11-30 11:18:25 +0100

    ye

--- e2.py
+++ e2.py
@@ -5,4 +5,9 @@ M03 UF1 pp4
 Descripció: Programa que mostra un triangle amb nombres a les cantonades.
 """
 
-alçada = int(input())
+alçada = int(input("Donam una alçada entre el 2 i el 9: "))
+count = 0
+for i in range(1, alçada + 1):
+    print(i * count)
+    count += 1
+
