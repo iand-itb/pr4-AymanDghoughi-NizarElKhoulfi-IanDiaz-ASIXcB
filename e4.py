965108e650c6e9856751a243c24b285710df000a
iand-itb <ian.diaz.7e5@itb.cat>
2023-11-30 09:57:22 +0100

    pelao

--- e4.py
+++ e4.py
@@ -3,8 +3,8 @@ NEGRE = "  "
 for i in range(1, 9):
     for j in range(1, 9):
         if (i + j) % 2 == 0:
-            print(NEGRE, end="")
-        else:
             print(BLANC, end="")
+        else:
+            print(NEGRE, end="")
         #print(" ", end="")
     print()
